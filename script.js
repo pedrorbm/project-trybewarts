98aaac15345a5eaacaf55f7238cf2b7d56908672
IgorSilvaJ <igorjsilvabiz@gmail.com>
2022-09-15 10:41:24 -0300

    Adiciona requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const formEmail = document.querySelector('#email');
 const formSenha = document.querySelector('#password');
 const btnSubmit = document.querySelector('#submit-btn');
 const checkAgree = document.querySelector('#agreement');
+const txtArea = document.querySelector('#textarea');
+const countChar = document.querySelector('#counter');
+const evalForm = document.querySelector('#evaluation-form');
+const hiddenForm = document.querySelector('#form-data');
 
 formLogin.lastElementChild.addEventListener('click', () => {
   if (formEmail.value === 'tryber@teste.com' && formSenha.value === '123456') {
@@ -19,3 +23,21 @@ checkAgree.addEventListener('click', () => {
     btnSubmit.disabled = true;
   }
 });
+
+txtArea.addEventListener('input', (input) => {
+  const target = input.currentTarget;
+  const maxLength = target.getAttribute('maxlength');
+  const currentLength = target.value.length;
+  countChar.innerHTML = `${maxLength - currentLength}`;
+});
+
+function styleForm(data) {
+  evalForm.style.display = 'none';
+  hiddenForm.style.flexDirection = 'column';
+  hiddenForm.style.display = 'flex';
+  for (let index = 0; index < hiddenForm.children.length; index += 1) {
+    hiddenForm.children[index].innerHTML += data[index];
+  }
+}
+
+styleForm('data');
